5e6e841c64727d684a1c6d9477232beccdfdd7d0
NJohnsonCR <naheemjohnson@estudiantec.cr>
2023-05-31 00:54:41 -0600

    Created register view

--- NutriTEC_MobileApp/NutriTEC_MobileApp/NutriTEC_MobileApp/Views/RegisterPage.xaml.cs
+++ NutriTEC_MobileApp/NutriTEC_MobileApp/NutriTEC_MobileApp/Views/RegisterPage.xaml.cs
@@ -16,5 +16,10 @@ namespace NutriTEC_MobileApp.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
     }
 }
