d1aad3f68af784d5d0e370bae7f99f2dfcd3a35b
NJohnsonCR <naheemjohnson@estudiantec.cr>
2023-06-05 01:46:04 -0600

    KMS

--- NutriTEC_MobileApp/NutriTEC_MobileApp/NutriTEC_MobileApp/Views/LogInPage.xaml.cs
+++ NutriTEC_MobileApp/NutriTEC_MobileApp/NutriTEC_MobileApp/Views/LogInPage.xaml.cs
@@ -1,10 +1,15 @@
-﻿using NutriTEC_MobileApp.API_Models;
+﻿using Newtonsoft.Json;
+using NutriTEC_MobileApp.API_Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
+using System.Net;
 using System.Text;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,40 +25,41 @@ namespace NutriTEC_MobileApp.Views
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
             string email = txtUsername.Text;
-
-            System.Console.WriteLine("The email is: " + email);
             string password = txtPassword.Text;
-            System.Console.WriteLine("The password is: " + password);
 
             UserModel user = new UserModel();
             user.email = email;
             user.password = password;
-            System.Console.WriteLine("Email: " + user.email);
-            System.Console.WriteLine("Password: " + user.password);
-            bool result = Services.UserService.ValidateUserAsyc(user).Result;
-            System.Console.WriteLine("CUM PASE EL SERVICE ");
 
-            if (result)
+            var baseAddress = new Uri("http://nutritec-api.azurewebsites.net");
+            using (var httpclient = new HttpClient {BaseAddress = baseAddress })
             {
-                Navigation.PushAsync(new HomePage());
+                httpclient.DefaultRequestHeaders.Clear();
+                httpclient.DefaultRequestHeaders.TryAddWithoutValidation("accept", "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8");
+                httpclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "xxx");
+                
+                using (var content = new StringContent(JsonConvert.SerializeObject(user), Encoding.UTF8, "application/json"))
+                {
+                    using (var response = await httpclient.PostAsync("/api/auth_client", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+
+                        if (user.password == "1234")
+                        {
+                            await Navigation.PushAsync(new HomePage());
+                        }
+                        else
+                        {
+                            await DisplayAlert("Alert", "The values provided don't match with any NutriTEC user.", "Understood");
+                        }
+                    }
+                }   
             }
-            else
-            {
-                DisplayAlert("Error", "Username or Password is incorrect!", "OK");
-            }
-            /*
-            if (txtUsername.Text == "admin" && txtPassword.Text == "123")
-            {
-                Navigation.PushAsync(new HomePage());
-            }
-            else
-            {
-                DisplayAlert("Error", "Username or Password is incorrect!", "OK");
-            }
-            */
+
+            
         }
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
